83d30446c76c9a389fd05b3dd1a2bdebb34df99c
Roman Hloska <roman.hloska@gmail.com>
2023-07-22 11:08:52 +0200

    iterate over vector and modify if necessary

--- Day_6/src/main.rs
+++ Day_6/src/main.rs
@@ -1,28 +1,48 @@
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 
+const DAYS: i32 = 80;
+
 fn main() {
     let filename = "src/input/lanternfish";
     let lines = read_file(filename);
-    println!("{:?}", lines);
 
-    let lf = parse_lanternfish(lines);
-    println!("{:?}", lf);
+    let mut lf = parse_lanternfish(lines);
+    println!("Initial state: {:?}", lf);
 
+    for _current_day in 0..DAYS {
+        process_day(&mut lf);
+        // println!("After {:?} day: {:?}", (_current_day + 1), lf);
+    }
+
+    println!("After {:?} there are {:?} lanternfish", DAYS , lf.len());
 }
 
 #[derive(Default, Debug)]
 struct Lanternfish {
-    age: u32,
-    def_days_to_new: u32,
+    counter: i32,
 }
 
 impl Lanternfish {
-    fn new(age: u32) -> Lanternfish {
+    fn new(counter: i32) -> Lanternfish {
         Lanternfish {
-            age,
-            def_days_to_new: 6,
+            counter,
         }
+    }
+
+    fn get_next_day(&mut self) -> i32 {
+        if self.counter > 0 {
+            self.counter-=1;
+            return self.counter;
+        }
+
+        self.reset();
+
+        -1
+    }
+
+    fn reset(&mut self) {
+        self.counter = 6;
     }
 }
 fn read_file(path: &str) -> Vec<String> {
@@ -38,10 +58,21 @@ fn parse_lanternfish(lines: Vec<String>) -> Vec<Lanternfish> {
     for l in lines {
         let numbers:Vec<&str> = l.split(",").collect();
         for number in numbers {
-            let mut lf = Lanternfish::new(number.parse().expect("Not a number!"));
+            let lf = Lanternfish::new(number.parse().expect("Not a number!"));
             lanternfish.push(lf);
         }
     }
 
     lanternfish
+}
+
+fn process_day(lanternfish: &mut Vec<Lanternfish>) {
+    for index_lf in 0..lanternfish.len() {
+        let internal_lf_counter = lanternfish[index_lf].get_next_day();
+
+        if internal_lf_counter == -1 {
+            let lf_born = Lanternfish::new(8);
+            lanternfish.push(lf_born);
+        }
+    }
 }
